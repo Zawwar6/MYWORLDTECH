dc1d638b8c4e6eb9339598f95cb12311da6ec151
Zawwar Khatri <159942012+Zawwar6@users.noreply.github.com>
2025-02-15 20:52:45 +0500

    Hello

--- src/components/Contacta/Contact.jsx
+++ src/components/Contacta/Contact.jsx
@@ -94,7 +94,7 @@ const Contact = () => {
       </form>
 
       {/* Toast Container for Notifications */}
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </div>
   );
 };
